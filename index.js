675c9a71d76b9f8941642d6e97acbff82c7763d6
SAYAN <sayanbasu91@gmail.com>
2014-09-08 10:15:45 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,127 @@
+1//
+var http = require('http'),
+	express=require('express'),
+	path=require('path'),
+	MongoClient=require('mongodb').MongoClient,
+	Server=require('mongodb').Server,
+	CollectionDriver = require('./collectionDriver').CollectionDriver;
+
+var app= express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','jade');
+
+app.use(express.bodyParser());
+
+// MongoDB incorporation
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017; 
+var collectionDriver;
+ 
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
+mongoClient.open(function(err, mongoClient) { //C
+  if (!mongoClient) {
+      console.error("Error! Exiting... Must start MongoDB first");
+      process.exit(1); //D
+  }
+  var db = mongoClient.db("MyDatabase");  //E
+  collectionDriver = new CollectionDriver(db); //F
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//  Posting data to database
+app.post('/:collection', function(req, res) { //A
+    var object = req.body;
+    var collection = req.params.collection;
+    collectionDriver.save(collection, object, function(err,docs) {
+          if (err) { res.send(400, err); } 
+          else { res.send(201, docs); } //B
+     });
+});
+
+
+
+// updation
+app.put('/:collection/:entity', function(req, res) { //A
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+       collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //C
+       });
+   } else {
+       var error = { "message" : "Cannot PUT a whole collection" };
+       res.send(400, error);
+   }
+});
+
+
+// similar to post but does not require body
+app.delete('/:collection/:entity', function(req, res) { //A
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+       collectionDriver.delete(collection, entity, function(error, objs) { //B
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //C 200 b/c includes the original doc
+       });
+   } else {
+       var error = { "message" : "Cannot DELETE a whole collection" };
+       res.send(400, error);
+   }
+});
+
+
+app.get('/:collection', function(req, res) { //A
+   var params = req.params; //B
+   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
+    	  if (error) { res.send(400, error); } //D
+	      else { 
+	          if (req.accepts('html')) { //E
+    	          res.render('data',{objects: objs, collection: req.params.collection}); //F
+              } else {
+	          res.set('Content-Type','application/json'); //G
+                  res.send(200, objs); //H
+              }
+         }
+   	});
+});
+ 
+
+/* When you define the /collection route in Express it will match “collection” exactly. 
+However, if you define the route as /:collection as in line A then it will match any 
+first-level path store the requested name in the req.params. collection in line B. 
+In this case, you define the endpoint to match any URL to a MongoDB collection using findAll 
+of CollectionDriver in line C. */
+
+app.get('/:collection/:entity', function(req, res) { //I
+   var params = req.params;
+   var entity = params.entity;
+   var collection = params.collection;
+   if (entity) {
+       collectionDriver.get(collection, entity, function(error, objs) { //J
+          if (error) { res.send(400, error); }
+          else { res.send(200, objs); } //K
+       });
+   } else {
+      res.send(400, {error: 'bad url', url: req.url});
+   }
+});
+ 
+ // catch-all callback
+app.use( function(req,res) {
+	res.render('404', {url:req.url});
+});
+
+
+
+//2 
+http.createServer(app).listen(app.get('port'),function (){
+	console.log('Express server listening on port' + app.get('port'))
+});
